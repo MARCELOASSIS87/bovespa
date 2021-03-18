508703a385ad3c70211e13cfe9c52b8c24a4d76b
Luiz Alberto <gomes.luiz@gmail.com>
2021-03-18 19:33:40 -0300

    test: exceção gerada para valores negativos.
    Uma exceção é gerada para qualquer valor negativo passado como parâmetro
    no construtor do Candlestick.
    
    Resolves: #8

--- acoes/src/main/java/br/pucpcaldas/acoes/dominio/Candlestick.java
+++ acoes/src/main/java/br/pucpcaldas/acoes/dominio/Candlestick.java
@@ -33,6 +33,10 @@ public class Candlestick {
     public Candlestick(double abertura, double fechamento, double minimo, double maximo, double volume,
             LocalDate data) {
 
+        if ((abertura < 0.0) || (fechamento < 0.0) || (minimo < 0.0) || (maximo < 0.0) || 
+            (volume < 0.0))
+            throw new IllegalArgumentException("valores negativos não são permitidos.");
+
         if (maximo < minimo)
             throw new IllegalArgumentException("preço máximo deve ser maior ou igual ao mínimo.");
 
--- acoes/src/test/java/br/pucpcaldas/acoes/dominio/CandlestickTest.java
+++ acoes/src/test/java/br/pucpcaldas/acoes/dominio/CandlestickTest.java
@@ -45,4 +45,34 @@ public class CandlestickTest {
         Candlestick umCandle = new Candlestick(50.0, 40.0, 35.0, 55.0, 1500.00, null);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void deveRetornarUmaExcecaoParaAberturaNegativa() {
+        // Arranjo, Ação, Asserção
+        Candlestick umCandle = new Candlestick(-50.0, 35.0, 40.0, 55.0, 1500.00, LocalDate.now());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deveRetornarUmaExcecaoParaFechamentoNegativo() {
+        // Arranjo, Ação, Asserção
+        Candlestick umCandle = new Candlestick(50.0, -35.0, 40.0, 55.0, 1500.00, LocalDate.now());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deveRetornarUmaExcecaoParaMinimoNegativo() {
+        // Arranjo, Ação, Asserção
+        Candlestick umCandle = new Candlestick(50.0, 35.0, -40.0, 55.0, 1500.00, LocalDate.now());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deveRetornarUmaExcecaoParaMaximoNegativo() {
+        // Arranjo, Ação, Asserção
+        Candlestick umCandle = new Candlestick(50.0, 35.0, 40.0, -55.0, 1500.00, LocalDate.now());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deveRetornarUmaExcecaoParaVolumeNegativo() {
+        // Arranjo, Ação, Asserção
+        Candlestick umCandle = new Candlestick(50.0, 35.0, 40.0, 55.0, -1500.00, LocalDate.now());
+    }
+
 }
